024e158da27a4521ed89105bd99443d4f87df223
Melissa Giannina Alvarado Baudat <N00234837@upn.pe>
2022-05-12 22:16:38 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo02/main.cpp
@@ -0,0 +1,13 @@
+//Crear una funcion que permita invertir un numero entero haciendo uso de una pila.
+
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
